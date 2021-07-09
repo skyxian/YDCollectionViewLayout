32ed3520f995c1f524cdf5a9bd562bfe6dc36ba7
咸宝坤 <xianbaokun@miliantech.com>
2021-07-09 10:17:48 +0800

    Initial commit

---
+++ Example/YDCollectionViewLayout/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  YDCollectionViewLayout
+//
+//  Created by 咸宝坤 on 07/09/2021.
+//  Copyright (c) 2021 咸宝坤. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
